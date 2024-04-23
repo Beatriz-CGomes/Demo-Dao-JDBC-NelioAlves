2cbe89bd2f0bb7bd33c8222c57a59e76e9a93ed9
Beatriz-CGOMES <Beatriz.cg15@hotmail.com>
2024-04-23 12:45:52 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -7,6 +8,6 @@ public class DaoFactory {
 	// vai ter operações statica para instanciar os dao
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
